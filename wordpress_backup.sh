86a24f0f4b06de3d382eb0e297115594de6e754f
Baytuch <dev@it-hobby.km.ua>
2023-04-04 07:56:23 +0300

    Information about the temporary folder

--- wordpress_backup.sh
+++ wordpress_backup.sh
@@ -9,3 +9,10 @@ TMP_DIR="tmp"
 BACKUP_TMP_DIR=""
 PROJECT_TMP_DIR=""
 BACKUP_DATE=$(date +"%Y-%m-%d")
+
+
+if [ $(echo $TMP_DIR | cut -b 1) = "/" ]; then
+  BACKUP_TMP_DIR=$TMP_DIR
+else
+  BACKUP_TMP_DIR=$BACKUP_ROOT_DIR/$TMP_DIR
+fi
